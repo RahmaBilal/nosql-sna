f2d2749d8ebbf7ad717c5b15ea92c1334631df39
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 03:07:09 +0000

    adding function to delete friend

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -99,3 +99,22 @@ const userController = {
         res.status(500).json(err);
       });
   },
+
+
+
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;
