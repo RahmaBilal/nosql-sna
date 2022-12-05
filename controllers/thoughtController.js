557074b31059e966c19ce5436a44c8ffed1a9b4c
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 17:32:28 +0000

    creating function for creating a new thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -13,7 +13,7 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
-  
+
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
       .then((dbThoughtData) => {
@@ -21,6 +21,28 @@ const thoughtController = {
           return res.status(404).json({ message: 'No thought found with this id!' });
         }
         res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'Thought created but no user found with this id!' });
+        }
+
+        res.json({ message: 'Thought successfully created!' });
       })
       .catch((err) => {
         console.log(err);
