d5875f5b5a6631b2bc4a753b9a25402952637834
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 03:06:45 +0000

    adding friend

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -84,3 +84,18 @@ const userController = {
         res.status(500).json(err);
       });
   },
+
+
+  addFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
