097cba24bbe8e3bd7410213472478f9376cff97e
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 17:39:36 +0000

    adding routes

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+router.use((req, res) => res.send('this is a wrong route!'));
+
+module.exports = router;
