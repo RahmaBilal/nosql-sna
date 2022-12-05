9f0b3163e3abad5819133007d5ebb994c6a75b67
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 17:38:19 +0000

    adding thoughts

---
+++ models/thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const reactionsSchema = require("./Reactions");
+const dateFormat = require("../utils/dateFormat");
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Leave a thought!",
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionsSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionsCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
