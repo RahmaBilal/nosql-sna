e09183337b2b368c11cf98a3c9d8f77d0781c415
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 17:43:27 +0000

    formatting date/time/year/month

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -35,4 +35,28 @@ const addDateSuffix = (date) => {
       10: monthLength === 'short' ? 'Nov' : 'November',
       11: monthLength === 'short' ? 'Dec' : 'December',
     };
+
+    const dateObj = new Date(timestamp);
+    const formattedMonth = months[dateObj.getMonth()];
   
+    const dayOfMonth = dateSuffix
+      ? addDateSuffix(dateObj.getDate())
+      : dateObj.getDate();
+  
+    const year = dateObj.getFullYear();
+    let hour =
+      dateObj.getHours() > 12
+        ? Math.floor(dateObj.getHours() - 12)
+        : dateObj.getHours();
+  
+    if (hour === 0) {
+      hour = 12;
+    }
+  
+  
+    const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+    const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
+    const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+  
+    return formattedTimeStamp;
+  };
