e7e07593fdb2d800161268dc49f15f176b092351
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 17:34:34 +0000

    adding function to remove reaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -108,3 +108,25 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  removeReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+
+module.exports = thoughtController;
