03d27e6df75ef0a48311751b45dca049d65cba1f
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 17:32:50 +0000

    updating thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -49,3 +49,17 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
