ba04ca3b7c70665b00dc358b4230f204cc4d46fe
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-05 17:33:38 +0000

    adding reaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -90,3 +90,21 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
