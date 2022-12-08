6488a82d5c7969a96b479728dc5750dacc84b008
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-08 04:40:47 +0000

    fixing errors

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString =  
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1/';
+const connectionString =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://rahma-bilal:B90wLbqKsK52fBrp@cluster0.1eogt36.mongodb.net/sna-db";
 
-mongoose.connect(connectionString, {  
-    useNewUrlParser: true,  
-    useUnifiedTopology: true,
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
 
 module.exports = mongoose.connection;
